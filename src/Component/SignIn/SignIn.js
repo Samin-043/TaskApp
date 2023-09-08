223d6e157b388fe981f7f38e756fb3cc51782287
samin-043 <saminsamin27998@gmail.com>
2023-09-08 21:38:46 +0600

    First Commit

---
+++ src/Component/SignIn/SignIn.js
@@ -0,0 +1,54 @@
+// src/SignInForm.js
+
+import React, { useState } from 'react';
+
+function SignInForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle sign in logic here
+    console.log('User trying to sign in:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">Sign In</button>
+      </div>
+    </form>
+  );
+}
+
+export default SignInForm;
